1407b287e253af905274ae5abd49f44e4859dd10
measmeng <measraim@usc.edu>
2020-12-11 09:07:12 -0800

    rank variables

--- scripts/combine_zip_with_program_info.R
+++ scripts/combine_zip_with_program_info.R
@@ -74,6 +74,52 @@
   
   setorder(matched_public, `Program ID`, `Year`)
   setorder(nonmatched_public, `Program ID`, `Year`)
+  
+# aggregate columns by program ID + year -------
+  
+  budget_claim = claims_public[, .(type = 'claim',
+                                   budget_usd = sum(`Budget`, na.rm = T),
+                                   gross_measure_cost_usd = sum(`Gross Measure Cost`, na.rm = T),
+                                   lifecycle_gross_kwh = sum(`Lifecycle Gross kWh`, na.rm = T)), 
+                               by = .(`Program ID`, `PA`, `Program Name`, `Primary Sector`, `Program Category`, `Program Implementer`, `Year`)]
+  budget_filing = filings_public[, .(type = 'filing',
+                                     budget_usd = sum(`Budget`, na.rm = T),
+                                     gross_measure_cost_usd = sum(`Gross Measure Cost`, na.rm = T),
+                                     lifecycle_gross_kwh = sum(`Lifecycle Gross kWh`, na.rm = T)), 
+                                 by = .(`Program ID`, `PA`, `Program Name`, `Primary Sector`, `Program Category`, `Program Implementer`, `Year`)]
+  
+# combine claims and filings data -------
+  
+  vars_combined = rbindlist(list(budget_claim, budget_filing), use.names = T)
+
+# get 2019 filings data ------
+  
+  vars_2019 = vars_combined[Year == 2019 & type == 'filing']
+  # claims_2019 = budget_claim[Year == 2019]
+  # filings_2019 = budget_filing[Year == 2019]
+  
+# get top 10 categories for filings ------
+  
+  vars_2019[, budget_rank := rank(-budget_usd)]
+  vars_2019[, measure_cost_rank := rank(-gross_measure_cost_usd)]
+  vars_2019[, gross_kwh_rank := rank(-lifecycle_gross_kwh)]
+  
+# merge ranking with variable data -------
+  
+  vars_combined = merge(vars_combined, 
+                        vars_2019[, .(`Program ID`, `PA`, `Program Name`, `Primary Sector`, `Program Category`, `Program Implementer`,
+                                      budget_rank, measure_cost_rank, gross_kwh_rank)], 
+                        on = c(`Program ID`, `PA`, `Program Name`, `Primary Sector`, `Program Category`, `Program Implementer`),
+                        all.x = T)
+  
+  vars_combined[, budget_program_name := ifelse(budget_rank %in% 1:10, `Program Name`, 'Other')]
+  vars_combined[, measure_cost_program_name := ifelse(measure_cost_rank %in% 1:10, `Program Name`, 'Other')]
+  vars_combined[, gross_kwh_program_name := ifelse(gross_kwh_rank %in% 1:10, `Program Name`, 'Other')]
+  
+# export to csvs --------
+  
+  fwrite(matched_public, file.path(save_path, 'public_programs_with_zip_code_data_2017_2019.csv'), row.names = F)
+  fwrite(nonmatched_public, file.path(save_path, 'public_programs_without_zip_code_data_2017_2019.csv'), row.names = F)
 
 # count unique number of programs in each dataset -------
   
@@ -87,8 +133,4 @@
   uniqueN(matched_public[, `Program ID`, Year])
   uniqueN(nonmatched_public[, `Program ID`, Year])
   
-# export to csvs --------
-  
-  fwrite(matched_public, file.path(save_path, 'public_programs_with_zip_code_data_2017_2019.csv'), row.names = F)
-  fwrite(nonmatched_public, file.path(save_path, 'public_programs_without_zip_code_data_2017_2019.csv'), row.names = F)
   
