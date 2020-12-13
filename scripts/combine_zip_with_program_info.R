edbfe10a96855ee2c2b3b312e60cbadadf520834
measmeng <measraim@usc.edu>
2020-12-12 23:09:26 -0800

    edit figures to show 2018 claims data only

--- scripts/combine_zip_with_program_info.R
+++ scripts/combine_zip_with_program_info.R
@@ -45,8 +45,8 @@
   
 # load public filings data -------
   
-  dt_filigs = fread(file.path(data_path, 'raw', filings_file), header = T)
-  filings_public = dt_filigs[`Primary Sector` == 'Public']
+  # dt_filigs = fread(file.path(data_path, 'raw', filings_file), header = T)
+  # filings_public = dt_filigs[`Primary Sector` == 'Public']
 
 # get unique set of programs and primary sector --------
   
@@ -81,53 +81,53 @@
   
 # aggregate columns by program ID + year -------
   
-  budget_claim = claims_public[, .(type = 'claim',
-                                   budget_usd = sum(`Budget`, na.rm = T),
-                                   gross_measure_cost_usd = sum(`Gross Measure Cost`, na.rm = T),
-                                   lifecycle_gross_kwh = sum(`Lifecycle Gross kWh`, na.rm = T)), 
-                               by = .(`Program Name`, `Primary Sector`, `Program Category`, `Program Implementer`, `Year`)]
-  budget_filing = filings_public[, .(type = 'filing',
-                                     budget_usd = sum(`Budget`, na.rm = T),
-                                     gross_measure_cost_usd = sum(`Gross Measure Cost`, na.rm = T),
-                                     lifecycle_gross_kwh = sum(`Lifecycle Gross kWh`, na.rm = T)), 
-                                 by = .(`Program Name`, `Primary Sector`, `Program Category`, `Program Implementer`, `Year`)]
+  vars_claim = claims_public[, .(budget_usd = sum(`Budget`, na.rm = T),
+                                 gross_measure_cost_usd = sum(`Gross Measure Cost`, na.rm = T),
+                                 lifecycle_gross_kwh = sum(`Lifecycle Gross kWh`, na.rm = T)), 
+                             by = .(`Program ID`, `Program Name`, `Primary Sector`, `Program Category`, `Program Implementer`, `Year`)]
+  # budget_filing = filings_public[, .(type = 'filing',
+  #                                    budget_usd = sum(`Budget`, na.rm = T),
+  #                                    gross_measure_cost_usd = sum(`Gross Measure Cost`, na.rm = T),
+  #                                    lifecycle_gross_kwh = sum(`Lifecycle Gross kWh`, na.rm = T)), 
+  #                                by = .(`Program Name`, `Primary Sector`, `Program Category`, `Program Implementer`, `Year`)]
   
 # combine claims and filings data -------
   
-  vars_combined = rbindlist(list(budget_claim, budget_filing), use.names = T)
+  # vars_combined = rbindlist(list(budget_claim, budget_filing), use.names = T)
   
 # calculate gross measure cost per kwh -------
   
-  vars_combined[, cost_per_kwh := gross_measure_cost_usd/lifecycle_gross_kwh]
+  vars_claim[, cost_per_kwh := gross_measure_cost_usd/lifecycle_gross_kwh]
   
-# get 2019 filings data ------
+# get 2018 claims data ------
   
-  vars_2019 = vars_combined[Year == 2019 & type == 'filing' & cost_per_kwh < Inf]
+  vars_2018 = vars_claim[Year == 2018 & cost_per_kwh < Inf]
+  # vars_2019 = vars_claim[Year == 2019 & cost_per_kwh < Inf]
   # claims_2019 = budget_claim[Year == 2019]
   # filings_2019 = budget_filing[Year == 2019]
   
-# get top 10 categories for filings ------
+# get top 10 categories for claims ------
   
-  vars_2019[, budget_rank := rank(-budget_usd)]
-  setorder(vars_2019, budget_rank)
-  top10_budget = vars_2019[1:10, `Program Name`]
+  vars_2018[, budget_rank := rank(-budget_usd)]
+  setorder(vars_2018, budget_rank)
+  top10_budget = vars_2018[1:10, `Program Name`]
   
-  vars_2019[, measure_cost_rank := rank(-gross_measure_cost_usd)]
-  setorder(vars_2019, measure_cost_rank)
-  top10_measure_cost = vars_2019[1:10, `Program Name`]
+  vars_2018[, measure_cost_rank := rank(-gross_measure_cost_usd)]
+  setorder(vars_2018, measure_cost_rank)
+  top10_measure_cost = vars_2018[1:10, `Program Name`]
   
-  vars_2019[, gross_kwh_rank := rank(-lifecycle_gross_kwh)]
-  setorder(vars_2019, gross_kwh_rank)
-  top10_gross_kwh = vars_2019[1:10, `Program Name`]
+  vars_2018[, gross_kwh_rank := rank(-lifecycle_gross_kwh)]
+  setorder(vars_2018, gross_kwh_rank)
+  top10_gross_kwh = vars_2018[1:10, `Program Name`]
   
-  vars_2019[, cost_per_kwh_rank := rank(cost_per_kwh)]
-  setorder(vars_2019, cost_per_kwh_rank)
-  top10_cost_per_kwh = vars_2019[1:10, `Program Name`]
+  vars_2018[cost_per_kwh > 0, cost_per_kwh_rank := rank(cost_per_kwh)]
+  setorder(vars_2018, cost_per_kwh_rank)
+  top10_cost_per_kwh = vars_2018[cost_per_kwh_rank %in% 1:10, `Program Name`]
 
 # merge ranking with variable data -------
   
-  vars_combined = merge(vars_combined, 
-                        vars_2019[, .(`Program Name`, `Primary Sector`, `Program Category`, `Program Implementer`,
+  vars_combined = merge(vars_claim, 
+                        vars_2018[, .(`Program ID`, `Program Name`, `Primary Sector`, `Program Category`, `Program Implementer`,
                                       budget_rank, measure_cost_rank, gross_kwh_rank, cost_per_kwh_rank)], 
                         on = c(`Program Name`, `Primary Sector`, `Program Category`, `Program Implementer`),
                         all.x = T)
@@ -139,19 +139,22 @@
   
 # aggregate variables ------
   
-  agg_measure_cost = vars_combined[, .(gross_measure_cost_usd = sum(gross_measure_cost_usd, na.rm = T)),
-                                   by = .(`measure_cost_program_name`, `Primary Sector`, `Program Category`, `Program Implementer`, `Year`, type)]
+  agg_measure_cost = vars_combined[, .(gross_measure_cost_usd = mean(gross_measure_cost_usd, na.rm = T)),
+                                   by = .(`measure_cost_program_name`, `Year`)]
+  agg_gross_kwh = vars_combined[, .(lifecycle_gross_kwh = mean(lifecycle_gross_kwh, na.rm = T)),
+                                   by = .(`gross_kwh_program_name`, `Year`)]
   agg_cost_per_kwh = vars_combined[, .(cost_per_kwh = mean(cost_per_kwh, na.rm = T)),
-                                   by = .(`cost_per_kwh_name`, `Primary Sector`, `Program Category`, `Program Implementer`, `Year`, type)]
+                                   by = .(`cost_per_kwh_name`, `Year`)]
   
 # reorder factor levels ------
   
-  vars_combined[, type := factor(type, levels = rev(c('claim', 'filing')))]
+  # vars_combined[, type := factor(type, levels = rev(c('claim', 'filing')))]
   vars_combined[, measure_cost_program_name := factor(measure_cost_program_name, levels = c(top10_measure_cost, 'Other'))]
   vars_combined[, gross_kwh_program_name := factor(gross_kwh_program_name, levels = c(top10_gross_kwh, 'Other'))]
   vars_combined[, cost_per_kwh_name := factor(cost_per_kwh_name, levels = c(top10_cost_per_kwh, 'Other'))]
   
-  agg_cost_per_kwh[, type := factor(type, levels = rev(c('claim', 'filing')))]
+  agg_measure_cost[, measure_cost_program_name := factor(measure_cost_program_name, levels = c(top10_measure_cost, 'Other'))]
+  agg_gross_kwh[, gross_kwh_program_name := factor(gross_kwh_program_name, levels = c(top10_gross_kwh, 'Other'))]
   agg_cost_per_kwh[, cost_per_kwh_name := factor(cost_per_kwh_name, levels = c(top10_cost_per_kwh, 'Other'))]
   
 # export to csvs --------
@@ -171,14 +174,12 @@
   uniqueN(matched_public[, `Program ID`, Year])
   uniqueN(nonmatched_public[, `Program ID`, Year])
   
-  
-  
 # ------------------------- plots ------------------------- 
   
   # plot theme & palettes ------
   
     pal_type = c('claim' = '#005581',
-                   'filing' = '#FFB511')
+                 'filing' = '#FFB511')
     
     theme_line = theme_ipsum(base_family = 'Secca Soft',
                              grid = 'Y', 
@@ -206,15 +207,15 @@
   
   # plot: measure costs ---------
   
-    fig_measure_cost = ggplot(vars_combined[Year == 2019], aes(x = measure_cost_program_name, y = gross_measure_cost_usd/1e6, group = type, fill = type)) +
-      geom_bar(position = "dodge", stat = "identity", width = 0.5) +
-      labs(title = 'Gross measure cost reported in 2019 filings',
+    fig_measure_cost = ggplot(agg_measure_cost[Year == 2018], aes(x = measure_cost_program_name, y = gross_measure_cost_usd/1e6)) +
+      geom_bar(position = "dodge", stat = "identity", width = 0.5, fill = '#005581') +
+      labs(title = 'Gross measure cost reported in 2018 claims',
            subtitle = 'Million USD',
-           caption = 'Non-top 10 programs aggregated into single program', 
+           caption = 'Other group is mean of gross measure cost of all non-top 10 programs', 
            x = NULL,
            y = NULL,
            fill = NULL) +
-      scale_fill_manual(values = pal_type) +
+      # scale_fill_manual(values = pal_type) +
       scale_x_discrete(labels = function(x) str_wrap(x, width = 10), expand = c(0,0)) + 
       scale_y_continuous(expand = c(0,0)) +
       theme_line
@@ -237,16 +238,14 @@
            dpi = 400, 
            units = 'in', 
            device = 'png')
-    
-    
-    
+
   # plot: lifecycle gross kwh ---------
     
-    fig_gross_kwh = ggplot(vars_combined[Year == 2019], aes(x = gross_kwh_program_name, y = lifecycle_gross_kwh/1e6, group = type, fill = type)) +
-      geom_bar(position = "dodge", stat = "identity", width = 0.5) +
-      labs(title = 'Lifecycle gross energy savings in 2019 filings',
+    fig_gross_kwh = ggplot(agg_gross_kwh[Year == 2018], aes(x = gross_kwh_program_name, y = lifecycle_gross_kwh/1e6)) +
+      geom_bar(position = "dodge", stat = "identity", width = 0.5, fill = '#005581') +
+      labs(title = 'Lifecycle gross energy savings in 2018 claims',
            subtitle = 'Terawatt-hours (TWh)',
-           caption = 'Non-top 10 programs aggregated into single program', 
+           caption = 'Other group is mean of lifecycle gross energy savings of all non-top 10 programs', 
            x = NULL,
            y = NULL,
            fill = NULL) +
@@ -274,15 +273,13 @@
            units = 'in', 
            device = 'png')
     
-    
-    
   # plot: measure cost per lifecycle savings kwh ---------
     
-    fig_cost_per_kwh = ggplot(agg_cost_per_kwh[Year == 2019], aes(x = cost_per_kwh_name, y = cost_per_kwh, group = type, fill = type)) +
-      geom_bar(position = "dodge", stat = "identity", width = 0.5) +
-      labs(title = 'Measure cost per lifecycle energy savings in 2019 filings',
+    fig_cost_per_kwh = ggplot(agg_cost_per_kwh[Year == 2018], aes(x = cost_per_kwh_name, y = cost_per_kwh)) +
+      geom_bar(position = "dodge", stat = "identity", width = 0.5, fill = '#005581') +
+      labs(title = 'Measure cost per lifecycle energy savings in 2018 claims',
            subtitle = 'USD per kWh',
-           caption = 'Non-top 10 programs aggregated into single program', 
+           caption = 'Other group is mean of cost per lifecycle energy savings of all non-top 10 programs', 
            x = NULL,
            y = NULL,
            fill = NULL) +
@@ -310,6 +307,3 @@
            units = 'in', 
            device = 'png')
     
-    
-    
-  
