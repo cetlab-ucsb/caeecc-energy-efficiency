ce9876188eb2614d9b04e9faeee5db15f80d3d8d
measmeng <measraim@usc.edu>
2021-02-10 15:45:44 -0800

    separate claims with bad county matches

--- scripts/analysis/aggregate_cpuc_county.R
+++ scripts/analysis/aggregate_cpuc_county.R
@@ -35,8 +35,11 @@
   
   dt_zip = fread(file.path(zip_path, zip_file), header = T, colClasses = rep("character", 24))
   dt_zip = dt_zip[STATE == '06']
-  dt_zip = dt_zip[, .(ZCTA5, COUNTY)]
+  dt_zip[, no_counties := uniqueN(COUNTY), by = ZCTA5]
+  dt_zip = dt_zip[, .(ZCTA5, COUNTY, no_counties)]
   dt_zip = unique(dt_zip)
+    
+  # no_counties_per_zip = dt_zip[, (no_counties = uniqueN(COUNTY)), by = ZCTA5]
   
 # import county fips code to county name -----
   
@@ -45,39 +48,46 @@
   dt_county = dt_county[state_fips == '06']
   dt_county = dt_county[, .(county_fips, area_name)]
   
+# take out claims with zip codes that belong to multiple counties ------
+  
+  claims_multiple_counties = dt_claims[ZCTA5 %in% unique(dt_zip[no_counties > 1, ZCTA5])]
+  claims_multiple_counties = claims_multiple_counties[unique(dt_zip[, .(ZCTA5, no_counties)]), on = .(ZCTA5), nomatch = 0]
+  
+# separate data table for claims with zip codes that only match one county ------
+  
+  dt_claims_single = dt_claims[!ZCTA5 %in% unique(dt_zip[no_counties > 1, ZCTA5])]
+  
 # combine claims data with zip code to county mapping ------
+
+  dt_claims_single_2 = merge(dt_claims_single, dt_zip, 
+                             by.x = 'ZCTA5',
+                             by.y = 'ZCTA5',
+                             all.x = T)
   
-  dt_claims_2 = merge(dt_claims, dt_zip, 
-                      by.x = 'ZCTA5',
-                      by.y = 'ZCTA5',
-                      all.x = T,
-                      all.y = F,
-                      allow.cartesian = T)
+  claims_no_county = dt_claims_single_2[is.na(COUNTY)]
   
-  dt_claims_2 = dt_claims_2[!is.na(COUNTY)]
+  dt_claims_single_2 = dt_claims_single_2[!is.na(COUNTY)]
   
   # dt_claims[!ZCTA5 %in% dt_zip[, ZCTA5]]
 
 # combine claims data with county name ------
   
-  dt_claims_3 = merge(dt_claims_2, dt_county, 
-                      by.x = 'COUNTY',
-                      by.y = 'county_fips',
-                      all.x = T,
-                      all.y = F,
-                      allow.cartesian = T)
+  dt_claims_single_3 = merge(dt_claims_single_2, dt_county, 
+                             by.x = 'COUNTY',
+                             by.y = 'county_fips',
+                             all.x = T)
   
 # aggregate by county ------
   
-  agg_claims = dt_claims_3[, lapply(.SD, sum, na.rm = TRUE), by = 'area_name', .SDcols = c('TotalFirstYearGrosskW',
-                                                                                           'TotalFirstYearGrosskWh',
-                                                                                           'TotalFirstYearGrossTherm',
-                                                                                           'TotalGrossIncentive',
-                                                                                           'TotalGrossMeasureCost',
-                                                                                           'TotalGrossMeasureCost_ER',
-                                                                                           'TotalLifecycleGrosskW',
-                                                                                           'TotalLifecycleGrosskWh',
-                                                                                           'TotalLifecycleGrossTherm') ] 
+  agg_claims = dt_claims_single_3[, lapply(.SD, sum, na.rm = TRUE), by = 'area_name', .SDcols = c('TotalFirstYearGrosskW',
+                                                                                                  'TotalFirstYearGrosskWh',
+                                                                                                  'TotalFirstYearGrossTherm',
+                                                                                                  'TotalGrossIncentive',
+                                                                                                  'TotalGrossMeasureCost',
+                                                                                                  'TotalGrossMeasureCost_ER',
+                                                                                                  'TotalLifecycleGrosskW',
+                                                                                                  'TotalLifecycleGrosskWh',
+                                                                                                  'TotalLifecycleGrossTherm') ] 
   
 # rename column ----
   
@@ -85,12 +95,25 @@
   
 # get unique claims-id + zip + county --------
   
-  dt_zip_county = unique(dt_claims_3[, .(PrgYear, ClaimID, PrgID, ZCTA5, COUNTY, area_name)])
+  dt_zip_county = unique(dt_claims_single_3[, .(PrgYear, ClaimID, PrgID, ZCTA5, COUNTY, area_name)])
   setnames(dt_zip_county, 'ZCTA5', 'zip_code')
   setnames(dt_zip_county, 'area_name', 'county_name')
+  
+# combine list of problematic zip codes and cities without counties ---------
+  
+  zip_problematic = rbindlist(list(claims_multiple_counties[, .(SiteCity, SiteZipCode)],
+                                   claims_no_county[, .(SiteCity, SiteZipCode)]))
+  
+  zip_problematic = unique(zip_problematic)
   
 # export to csv -------
 
   fwrite(agg_claims, file.path(save_path, save_file), row.names = F)
   fwrite(dt_zip_county, file.path(save_path, 'claims_zip_county_mapping.csv'), row.names = F)
+  
+  fwrite(claims_no_county, file.path(save_path, "cpuc_claims_without_matched_counties.csv"), row.names = F)
+  fwrite(claims_multiple_counties, file.path(save_path, "cpuc_claims_zip_codes_with_multiple_counties.csv"), row.names = F)
+  
+  fwrite(zip_problematic, file.path(save_path, "cities_and_zips_manual_match.csv"), row.names = F)
+  
   
