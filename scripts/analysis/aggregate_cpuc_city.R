9d677e1287a8de335f4195043f047cdae4b335d6
measrainsey <measrainsey@gmail.com>
2021-03-15 18:27:24 -0700

    remove k-12 programs from city aggregation

--- scripts/analysis/aggregate_cpuc_city.R
+++ scripts/analysis/aggregate_cpuc_city.R
@@ -5,9 +5,7 @@
 # ------------------------------ inputs ------------------------------ 
 
   data_path   = '/Volumes/GoogleDrive/.shortcut-targets-by-id/1-BTh8T0PMwHDs3KZ9V--KPrNWRUgRxvV/2020_CAEECC_Public_Sector_Underserved/data/processed/cpuc'
-  sel_data    = 'cpuc_claims_selected_programs_2017_2019.csv'
-  k12_data    = 'cpuc_claims_k-12_programs_2017_2019.csv'
-  zip_path    = '/Volumes/GoogleDrive/.shortcut-targets-by-id/1-BTh8T0PMwHDs3KZ9V--KPrNWRUgRxvV/2020_CAEECC_Public_Sector_Underserved/data/raw/census'
+  data_file   = 'cpuc_claims_selected_programs_2017_2019.csv'
 
 # outputs -------
 
@@ -21,26 +19,21 @@
   library(data.table)
   library(stringr)
 
-# load selected programs data ----
+# import claims data ------
   
-  dt_sel = fread(file.path(data_path, sel_data), header = T)
-  dt_sel[, c('Selected', 'Grouping') := NULL]
+  dt_claims = fread(file.path(data_path, data_file), header = T)
+
+# keep only local governments ------
   
-# load k-12 data ------
-  
-  dt_k12 = fread(file.path(data_path, k12_data), header = T)
-  
-# combine selected programs data and k-12 data -----
-  
-  dt_all = rbind(dt_sel, dt_k12, use.names = T)
+  dt_claims = dt_claims[Grouping %like% 'Local Government']
   
 # normalize city names -----
   
-  dt_all[, SiteCity := str_to_title(SiteCity)]
+  dt_claims[, SiteCity := str_to_title(SiteCity)]
   
 # aggregate by city -----
   
-  agg_city = dt_all[, lapply(.SD, sum, na.rm = TRUE), by = .(SiteCity), .SDcols = c('TotalFirstYearGrosskW',
+  agg_city = dt_claims[, lapply(.SD, sum, na.rm = TRUE), by = .(SiteCity), .SDcols = c('TotalFirstYearGrosskW',
                                                                                     'TotalFirstYearGrosskWh',
                                                                                     'TotalFirstYearGrossTherm',
                                                                                     'TotalGrossIncentive',
@@ -53,5 +46,5 @@
   
 # export to csv -------
   
-  fwrite(agg_city, file.path(save_path, save_file), row.names = F)
+  # fwrite(agg_city, file.path(save_path, save_file), row.names = F)
   
